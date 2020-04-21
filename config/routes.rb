dbe3c4529f4c72a8fc5573252aaa6f93198c1294
junya30st <ji06140210@gmail.com>
2020-04-21 14:44:00 +0900

    shop-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :customers
   resources :users
+  resources :shops
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
