2c981b9a7cf34104e55e495901b3c45f7ef9741b
junya30st <ji06140210@gmail.com>
2020-04-26 21:50:03 +0900

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   resources :customers
   resources :users
-  resources :shops do
-    resources :products
-  end
+  resources :shops
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :product do
-    
+    name                    {"居酒屋"}
+    introduction            {"こんにちは"}
+    price                   {1000}
+    # limit                   {}
+    association :shop
   end
 end
