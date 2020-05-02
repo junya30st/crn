6df0b06e0703cebb7bf0f606fb7afa4688d778b1
junya30st <ji06140210@gmail.com>
2020-05-02 23:41:21 +0900

    ビューの修正

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,11 @@
+// document.getElementById("header-menu").onclick = function() {
+//   console.log("ok")
+//   const lists = document.getElementById("menu-lists");
+//   const GetDisplay = window.getComputedStyle(lists, null).getPropertyValue('display');
+//   if (GetDisplay == 'none') {
+//     lists.style.display = 'block';
+//   } else {
+//     lists.style.display = 'none';
+//   }
+// };
+
