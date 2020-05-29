6b8b2290d46f71b1f24fd77ede3a1b7ce317438d
junya30st <ji06140210@gmail.com>
2020-05-30 08:24:16 +0900

    menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,6 @@
 class Menu < ApplicationRecord
   belongs_to :shop
+
+  validates :name, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/}
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Menu do
+    describe '#create' do
+
+      context 'can save' do
+
+        it '全て入力されていれば登録できる' do
+          menu = build(:menu)
+          expect(menu).to be_valid
+        end
+
+        it 'introductionがなくても登録できる' do
+          menu = build(:menu, introduction: "")
+          expect(menu).to be_valid
+        end
+
+      end
+
+      context 'can not save' do
+
+        it 'nameがないと登録できない' do
+          menu = build(:menu, name: "")
+          menu.valid?
+          expect(menu.errors[:name]).to include("を入力してください")
+        end
+
+        it 'priceがないと登録できない' do
+          menu = build(:menu, price: "")
+          menu.valid?
+          expect(menu.errors[:price]).to include("を入力してください")
+        end
+
+        # it 'priceが全角数字では登録できない' do
+        #   menu = build(:menu, price: "１００")
+        #   menu.valid?
+        #   expect(menu.errors[:price]).to include("は半角数字で入力してください")
+        # end
+      end
+    end
+  end
 end
