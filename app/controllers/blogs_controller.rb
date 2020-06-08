3083b0f40b4e30027ded176bc0a859a885aac286
junya30st <ji06140210@gmail.com>
2020-06-08 22:40:33 +0900

    ブログ投稿時の処理修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,8 +18,10 @@ class BlogsController < ApplicationController
       image: blog_params[:image],
       shop_id: @shop.id)
       
-    if @blog.save
-      @client.update("#{@shop.name}がfannaruでブログを更新しました。\r#{@blog.title}\r#{blog_url(@blog.id)}")
+    if @blog.save && current_user.token.present?
+      @client.update("#{@shop.name}が #fannaru でブログを更新しました。\r#{@blog.title}\r#{blog_url(@blog.id)}")
+      redirect_to blog_path(@blog), notice: '投稿に成功しました'
+    elsif @blog.save
       redirect_to blog_path(@blog), notice: '投稿に成功しました'
     else
       flash.now[:alert] = '投稿に失敗しました'
