7804e5a467255098dc5b4fb2f7b4ea46af4e8049
junya30st <ji06140210@gmail.com>
2020-08-02 22:37:57 +0900

    Update scroll.js

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -5,7 +5,7 @@ $(function() {
     contentSelector: '.shop-index__wrapper__fix--right--contents', 
     // 次のページにいくためのリンクの場所は？ ＞aタグの指定
     nextSelector: 'a.next',
-    // 読み込み中の表示はどうする？
+
     loadingHtml: '読み込み中'
   });
 });
