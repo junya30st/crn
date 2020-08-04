8b6be3180b5470bc19b419ad921c6dc3f8cb85f5
junya30st <ji06140210@gmail.com>
2020-08-04 23:29:02 +0900

    Update scroll.js

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -3,7 +3,7 @@ $(function() {
   $('.shop-index__wrapper__fix--right--contents').jscroll({
     // 無限に追加する要素は、どこに入れる？
     contentSelector: '.shop-index__wrapper__fix--right--contents', 
-    // 次のページにいくためのリンクの場所は？ ＞aタグの指定
+
     nextSelector: 'a.next',
 
     loadingHtml: '読み込み中'
