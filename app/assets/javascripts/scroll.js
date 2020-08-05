336494a63140a6642ed3fbf12cf39d2806bdcd72
junya30st <ji06140210@gmail.com>
2020-08-05 23:43:26 +0900

    Update scroll.js

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,7 @@
 $(function() {
   console.log("ok")
   $('.shop-index__wrapper__fix--right--contents').jscroll({
-    // 無限に追加する要素は、どこに入れる？
+    
     contentSelector: '.shop-index__wrapper__fix--right--contents', 
 
     nextSelector: 'a.next',
